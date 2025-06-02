42788ef6a0c34d6cbf337ed722ffa4eba4f34bd8
NotACat1 <notacat.mail@gmail.com>
2025-06-02 23:07:33 +0300

    fix(config): fixing CommonJS and Modules

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -2,5 +2,5 @@ module.exports = {
   '*.{js,ts,vue}': ['eslint --fix --max-warnings=0', 'prettier --write', 'vue-tsc --noEmit'],
   '*.{css,scss}': ['prettier --write'],
   '*.{json,md,yaml,yml}': ['prettier --write'],
-  '*.{png,jpeg,jpg,gif,svg}': ['imagemin-lint-staged'],
+  '*.{png,jpg,jpeg,gif,svg,webp}': ['imagemin-lint-staged'],
 }
